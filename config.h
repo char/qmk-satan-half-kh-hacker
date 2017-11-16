6f214f88c12cdc03b08d726f160c428d32ba6de3
half cambodian hacker man <anthonyjoshuasom@gmail.com>
2017-11-16 21:20:08 +0000

    Tweak mousekey settings

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 
 #include "config_common.h"
 
-#define MOUSEKEY_DELAY 1
-#define MOUSEKEY_INTERVAL 20
-#define MOUSEKEY_MAX_SPEED 2
+#define MOUSEKEY_DELAY 50
+#define MOUSEKEY_INTERVAL 5
+#define MOUSEKEY_MAX_SPEED 1
 #define MOUSEKEY_TIME_TO_MAX 0
 
 #define MOUSEKEY_WHEEL_MAX_SPEED 1
