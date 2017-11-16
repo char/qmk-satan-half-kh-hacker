f706e5e612e5ed9d76acc49ad7ef8ae6d14b9d6c
half cambodian hacker man <anthonyjoshuasom@gmail.com>
2017-11-16 13:38:36 +0000

    Add mouse keys to intern layer

--- config.h
+++ config.h
@@ -8,6 +8,9 @@
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_TIME_TO_MAX 0
 
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+
 #define MANUFACTURER    half-kh-hacker
 #define PRODUCT         ISO60
 
